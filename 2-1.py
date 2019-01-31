5dcea6771cfa0111297adb1c24c72ee0a0a9f53c
Kjell Vos <vos.kjell@gmail.com>
2019-01-31 17:51:16 +0100

    First upload, Practice 'Crash course Programmeren in Python'

---
+++ 2-1.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+	message = "Welcome to Jackass Programming"
+	print(message)
---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+	message = "Hello Python World!"
+	print(message)
+
+	message = "Hello Python Crash Courese World!"
+	print(message)
